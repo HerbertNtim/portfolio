4d8dfaaa5168da6c0519cd5ccb02d33dbcd6d95c
Herbert Ntim <herbertntim15@gmail.com>
2023-11-11 04:19:50 +0000

    bird animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
-export default function App() {
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+
+import Navbar from './components/Navbar'
+import { About, Contact, Home, Projects } from "./pages"
+
+const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <main className="bg-slate-300/20">
+      <BrowserRouter>
+        <Navbar />
+
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/about" element={<About />}/>
+          <Route path="/projects" element={<Projects />}/>
+          <Route path="/contact" element={<Contact />}/>
+        </Routes>
+      </BrowserRouter>
+    </main>
   )
 }
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import { NavLink } from "react-router-dom"
+
+const Navbar = () => {
+  return (
+    <header className="header">
+      <NavLink to='/' className='w-10 h-10 items-center justify-center flex font-bold shadow-md rounded-lg bg-white'>
+        <p className="blue-gradient_text">HN</p>
+      </NavLink>
+
+      <nav className="flex text-lg gap-7 font-medium">
+        <NavLink to={'/about'} className={({ isActive }) => isActive ? 'text-blue-500' : "text-black"}>
+          About
+        </NavLink>
+        <NavLink to='/projects' className={({ isActive }) => isActive ? 'text-blue-500' : 'text-black'}>
+          Projects          
+        </NavLink>
+      </nav>
+    </header>
+  )
+}
+
+export default Navbar
