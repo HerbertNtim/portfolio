7aeb57296997b048b2a8357be9d6c900506e2a7c
Herbert Ntim <herbertntim15@gmail.com>
2024-01-23 13:22:51 +0000

    footer updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,15 +3,17 @@ import { Link } from "react-router-dom";
 import { socialLinks } from "../constants";
 
 const Footer = () => {
+  const currentYear = new Date()
+
   return (
     <footer className='footer font-poppins'>
       <hr className='border-slate-200' />
 
       <div className='footer-container'>
         <p>
-          © 2023 <strong>Herbert Ntim</strong>. All rights reserved.
+          © {currentYear.getFullYear()} <strong>Herbert Ntim</strong>. All rights reserved.
         </p>
-
+        
         <div className='flex gap-3 justify-center items-center'>
           {socialLinks.map((link) => (
             <Link key={link.name} to={link.link} target='_blank'>
